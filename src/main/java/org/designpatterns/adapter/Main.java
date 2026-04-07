01c1dbcc251e9fe0db7453cde13490a04372d210
Eugene Shin <tdw0312@gmail.com>
2026-04-07 16:49:38 +0900

    feat: implement FileProperties adapter for FileIO

--- src/main/java/org/designpatterns/adapter/Main.java
+++ src/main/java/org/designpatterns/adapter/Main.java
@@ -1,5 +1,7 @@
 package org.designpatterns.adapter;
 
+import java.io.IOException;
+
 public class Main {
     public static void main(String[] args) {
         //Class Adapter
@@ -11,5 +13,18 @@ public class Main {
         AbstractPrint p2 = new PrintBannerObjectAdapter("Hello");
         p2.printWeak();
         p2.printStrong();
+
+        //FileIO-Properties Adapter
+        FileIO f = new FileProperties();
+        try {
+            f.readFromFile("src/main/java/org/designpatterns/adapter/file.txt");
+            f.setValue("width", "1024");
+            f.setValue("height", "512");
+            f.setValue("depth", "32");
+            f.writeToFile("src/main/java/org/designpatterns/adapter/newfile.txt");
+            System.out.println("newfile.txt is created.");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
