6b2f5131f1ed7562cd9aa10fe1a49baad3ba8b47
Eugene Shin <tdw0312@gmail.com>
2026-04-16 14:22:25 +0900

    feat: implement multiton pattern

---
+++ src/main/java/org/designpatterns/singleton/Triple.java
@@ -0,0 +1,25 @@
+package org.designpatterns.singleton;
+
+import java.util.NoSuchElementException;
+
+public class Triple {
+    private String name;
+    private static Triple alpha = new Triple("ALPHA");
+    private static Triple beta = new Triple("BETA");
+    private static Triple gamma = new Triple("GAMMA");
+
+    private Triple(String name) {
+        this.name = name;
+    }
+
+    public static Triple getInstance(String name) {
+        if("ALPHA".equals(name)) {
+            return alpha;
+        } else if("BETA".equals(name)) {
+            return beta;
+        } else if("GAMMA".equals(name)){
+            return gamma;
+        }
+        throw new NoSuchElementException();
+    }
+}
